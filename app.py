eba85c1b5aa52273d3a50ce8abef15267c147f18
davaparma <136113497+davaparma@users.noreply.github.com>
2024-09-01 15:35:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 import unittest
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_sit223():
     return "Hello SIT223!"
 
-# Your Python test functions
+# Unit Tests
 class TestSmartBin(unittest.TestCase):
 
     def test_bin_level(self):
@@ -19,11 +18,11 @@ class TestSmartBin(unittest.TestCase):
         waste_level = 0
         self.assertEqual(waste_level, 0, "Bin should be empty")
 
-
 if __name__ == '__main__':
-    # Check if we're running in a test environment or locally
-    if os.getenv('RUN_TESTS') == '1':
-        # Running the tests
+    import sys
+    if 'test' in sys.argv:
+        # Run unit tests
         unittest.main()
     else:
+        # Run the Flask app
         app.run(host='0.0.0.0', port=80)
