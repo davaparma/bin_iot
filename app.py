e1f9fcec0fd88ce255499bcce4f44fbfbca8a0c3
davaparma <136113497+davaparma@users.noreply.github.com>
2024-09-01 20:07:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_sit223():
-    return "Howdy - SIT223!"
+    return "Hello - SIT223!"
 
 class TestSmartBin(unittest.TestCase):
     def test_bin_level(self):
